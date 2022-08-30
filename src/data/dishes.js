56c20457628b7b221793bd140470dd8184aa0197
Julfikar <julfikarali.282@gmail.com>
2022-08-30 17:47:11 +0600

    Printing menu item

---
+++ src/data/dishes.js
@@ -0,0 +1,133 @@
+const DISHES = [
+    {
+        id: 0,
+        name: 'Ginger Barger',
+        image: 'assets/images/0burger.jpg',
+        category: 'meal',
+        label: 'Hot',
+        price: '199',
+        description: 'A unique combination of bangladeshi and italian pizza topped with cerigola olives, ripe vine chery tomatoes, vidalia onion, guntur chillies and buffalo paneer.',
+        comments: [
+            {
+                id: 0,
+                ratings: 5,
+                comment: "Imagine all the eatables, living in confusion",
+                author: "Shanto",
+                date: "2022-09-16T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                ratings: 4,
+                comment: "Sends anyone to heaven, I wish I could get my mother in law to eat it!",
+                author: "Karim",
+                date: "2021-07-05T17:57:28.556094Z"
+            },
+            {
+                id: 2,
+                ratings: 3,
+                comment: "Eat it just eat it!",
+                author: "Mominul",
+                date: "2021-08-05T17:57:28.556094Z"
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: 'Chicken Bar B Q Pizza',
+        image: 'assets/images/1pizza.jpg',
+        category: 'meal',
+        label: '',
+        price: '599',
+        description: 'Deep fried zucchi cerigola olives, ripe vine chery tomatoes, vidalia onion, guntur chillies and buffalo paneer.',
+        comments: [
+            {
+                id: 0,
+                ratings: 5,
+                comment: "Imagine all the eatables, living in confusion",
+                author: "Aman",
+                date: "2022-09-16T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                ratings: 4,
+                comment: "Sends anyone to heaven, I wish I could get my mother in law to eat it!",
+                author: "Momin",
+                date: "2021-07-05T17:57:28.556094Z"
+            },
+            {
+                id: 2,
+                ratings: 3,
+                comment: "Eat it just eat it!",
+                author: "Khan",
+                date: "2021-08-05T17:57:28.556094Z"
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'French Fries',
+        image: 'assets/images/2fries.jpg',
+        category: 'meal',
+        label: '',
+        price: '120',
+        description: 'A quintessential confusion experience, ripe vine chery tomatoes, vidalia onion, guntur chillies and buffalo paneer.',
+        comments: [
+            {
+                id: 0,
+                ratings: 5,
+                comment: "Imagine all the eatables, living in confusion",
+                author: "Shamim",
+                date: "2022-09-16T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                ratings: 4,
+                comment: "Sends anyone to heaven, I wish I could get my mother in law to eat it!",
+                author: "Rubel",
+                date: "2021-07-05T17:57:28.556094Z"
+            },
+            {
+                id: 2,
+                ratings: 3,
+                comment: "Eat it just eat it!",
+                author: "Mamun",
+                date: "2021-08-05T17:57:28.556094Z"
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'Cup Cake',
+        image: 'assets/images/3cake.jpg',
+        category: 'meal',
+        label: '',
+        price: '230',
+        description: 'A cake is good for taste ripe vine chery tomatoes, vidalia onion, guntur chillies and buffalo paneer.',
+        comments: [
+            {
+                id: 0,
+                ratings: 5,
+                comment: "Imagine all the eatables, living in confusion",
+                author: "Moin",
+                date: "2022-09-16T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                ratings: 4,
+                comment: "Sends cake anyone to heaven, I wish I could get my mother in law to eat it!",
+                author: "Rubel",
+                date: "2021-07-05T17:57:28.556094Z"
+            },
+            {
+                id: 2,
+                ratings: 3,
+                comment: "Cake is Eat it just eat it!",
+                author: "Rahman",
+                date: "2021-08-05T17:57:28.556094Z"
+            }
+        ]
+    }
+];
+
+
+export default DISHES;
